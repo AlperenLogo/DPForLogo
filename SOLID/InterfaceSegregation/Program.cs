6df937e4c4f34b62f1093f20c0f96ca9afd0c404
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-09-15 11:10:07 +0300

    Continuing

---
+++ SOLID/InterfaceSegregation/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace InterfaceSegregation
+{
+    internal class Program
+    {
+        //İşlevler; tek bir interface'e zorlanmamalı! Belirli işlevler belirli interface'lere ayrılmalı.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+
+    }
+
+
+    public interface IMessage
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+
+
+
+
+    }
+
+    public interface IVideoMessage : IMessage
+    {
+        public string VideoFormat { get; set; }
+        public int VideoDuration { get; set; }
+    }
+
+    public interface IAudioMessage : IMessage
+    {
+        public string AudioFormat { get; set; }
+        public int AudioDuration { get; set; }
+    }
+
+    public class TextMessage : IMessage
+    {
+        public string From { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string To { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Subject { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Body { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    }
+
+    public class VideoMessage : IVideoMessage
+    {
+        public string VideoFormat { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int VideoDuration { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string From { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string To { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Subject { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Body { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+
+
+    public class AudioMessage : IAudioMessage
+    {
+        public string AudioFormat { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int AudioDuration { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string From { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string To { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Subject { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Body { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ SOLID/LiskovSubstution/Program.cs
@@ -0,0 +1,101 @@
+﻿namespace LiskovSubstution
+{
+    internal class Program
+    {
+        /*
+         * Bir derived class; bir base class'ın davranışını değiştiremez. Yani, base ve derived birbirlerinin yerine kullanılabilir olmalıdır.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var rect = Factory.GetRectangle(7);
+
+            Console.WriteLine(rect.GetArea());
+
+
+
+        }
+    }
+
+
+
+    public static class Factory
+    {
+        public static IAreaCalcutable GetRectangle(int width, int height = 1)
+        {
+            //bir biçimde...........
+            if (height > 1)
+            {
+                return new Rectangle { Width = width, Height = height };
+            }
+            return new Square { UnitLength = width };
+        }
+    }
+
+    public interface IAreaCalcutable
+    {
+        int GetArea();
+    }
+    public class Rectangle : IAreaCalcutable
+    {
+
+        protected int width;
+        protected int height;
+        public int Width { get => width; set => width = value; }
+        public int Height { get => height; set => height = value; }
+
+        public int GetArea() => Width * Height;
+    }
+
+    public class Square : IAreaCalcutable //: Rectangle
+    {
+        public int UnitLength { get; set; }
+        public int GetArea() => UnitLength * UnitLength;
+
+
+    }
+
+
+    public class Scenerio
+    {
+        public void SetSource(DataSource dataSource)
+        {
+            dataSource.WriteData();
+        }
+
+        public DataSource GetDataSource(int number)
+        {
+            var isEven = number % 2 == 0;
+            return isEven ? new XMLDataSource() : new DBDataSource();
+
+        }
+    }
+
+    public class DataSource
+    {
+        public void GenerateReport()
+        {
+
+        }
+        public void WriteData()
+        {
+            Console.WriteLine("Ok");
+        }
+    }
+
+    public class XMLDataSource : DataSource
+    {
+
+    }
+
+    public class DBDataSource : DataSource
+    {
+
+    }
+
+
+
+
+
+}
