ea864cc6f25f4d450f7edff10f4a91baf0a368bb
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-09-15 11:59:47 +0300

    SOLID explained

---
+++ SOLID/DependencyInversion/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Bir nesne ...... olmadan çalışmıyor ise o ....... ' dependent'dir.
+
+public class ReportService
+{
+    ISender sender;
+
+    public ReportService(ISender sender)
+    {
+        sender = sender;
+    }
+    public void Send()
+    {
+
+        sender.Send();
+    }
+
+    public void DoSomething()
+    {
+        sender.Send();
+
+    }
+}
+
+
+public interface ISender
+{
+    void Send();
+}
+public class MailSender : ISender
+{
+    public void Send() => Console.WriteLine("Mail gönderildi");
+}
+
+public class WhatsappSender : ISender
+{
+    public void Send()
+    {
+        Console.WriteLine("WS ile gönderildi");
+    }
+}
+
+
